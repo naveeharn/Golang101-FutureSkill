d372e91542f2cf9460f79f5cce74926dd983f230
naveeharn <naveeharn@hotmail.com>
2022-12-03 23:57:54 +0700

    workshop

---
+++ go04-variable/workshop.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func workshop() {
+	workshop := "Println"
+	name := "Avengers : Endgame"
+	year := 2019
+	rating := 8.4
+	movie_type := "SciFi"
+	superhero := true
+	string :=
+		`
+1
+2
+3
+`
+
+	fmt.Println("Workshop:", workshop)
+	fmt.Println("Output:")
+	fmt.Println("name:", name)
+	fmt.Println("year:", year)
+	fmt.Println("rating:", rating)
+	fmt.Println("movie_type:", movie_type)
+	fmt.Println("superhero:", superhero)
+	fmt.Println(string)
+	fmt.Println("\nTODO: write code below.:")
+}
