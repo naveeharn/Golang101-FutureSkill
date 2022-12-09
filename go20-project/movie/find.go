e5e4e7b9582efab376537ad875c05349fc446074
naveeharn <naveeharn@hotmail.com>
2022-12-09 12:23:07 +0700

    go20 package find

---
+++ go20-project/movie/find.go
@@ -0,0 +1,18 @@
+package movie
+
+import "fmt"
+
+func init() {
+	fmt.Println("init: find")
+}
+
+// package movie
+func FindName(imdbID string) string {
+	switch imdbID {
+	case "tt4154796":
+		return "Avenger: Endgame"
+	case "tt3852683":
+		return "Black Panther"
+	}
+	return "not found"
+}
