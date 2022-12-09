e5038a7835ca29f833bd49e9f042f3c19e3248fd
naveeharn <naveeharn@hotmail.com>
2022-12-09 12:22:40 +0700

    go20 main

---
+++ go20-project/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/naveeharn/golang101/go20-project/movie"
+	"github.com/naveeharn/golang101/go20-project/ticket"
+)
+
+func init() {
+	fmt.Println("init: main")
+}
+
+func main() {
+	movieName := movie.FindName("tt4154796")
+	fmt.Println(movieName)
+	movie.Review(movieName, 7.7)
+	ticket.Buy(movieName)
+
+	fmt.Printf("\n")
+	fmt.Println()
+}
